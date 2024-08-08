c3d2ab5d520778b73912b08fff0f1d39faa88dd6
vivian.yang <vivian.yang@appier.com>
2024-08-08 10:33:35 +0800

    modify identify log

--- script.js
+++ script.js
@@ -224,14 +224,7 @@ log = function () {
       'username': username,
       'birthday': birthday
   };
-  const valuesWithContent = [];
-  for (const field in custom_attributes) {
-      if (custom_attributes[field] !== '') {
-          // 將有內容的欄位值和對應的標題組成字串，加入到 valuesWithContent 陣列中
-          valuesWithContent[field] = custom_attributes[field];
-      }
-  }
-  appier('identify', valuesWithContent);
+  appier('identify', custom_attributes);
 }
 
 
